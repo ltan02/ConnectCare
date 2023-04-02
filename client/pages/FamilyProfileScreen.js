4080136aeffa3beb52e6ccae88dfe9383d25ef0b
yui-ubc <yui.mikuriya@gmail.com>
2023-04-01 21:42:42 -0700

    Added Background and top component

--- client/pages/FamilyProfileScreen.js
+++ client/pages/FamilyProfileScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 
 const FamilyProfileScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World 3</Text>
-    </View>
+    <LinearGradient colors={[`rgba(131, 255, 255, 0.45)`, `rgba(0, 187, 121, 0.45)`]}
+    style={styles.linearGradient}
+    >
+      <View style={styles.progressBarBox}></View>
+
+    </LinearGradient>
   );
 };
 
@@ -15,6 +20,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  linearGradient: {
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  progressBarBox: {
+    backgroundColor: "#F2FFFD",
+    height: 195,
+    paddingHorizontal: 20,
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 20,
+    marginBottom: 30,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 20,
+    elevation: 1,
   },
 });
 
